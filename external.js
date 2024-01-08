c0e8857466134aa55bb21d85954116910eed8cb6
MadhumithaBalasubramani <113843707+MadhumithaBalasubramani@users.noreply.github.com>
2024-01-08 20:52:57 +0530

    Add files via upload

---
+++ external.js
@@ -0,0 +1,60 @@
+// alert("Introduction of Javascript");
+// prompt("Enter your name");
+// confirm("Are you degree holder");
+console.log("javascript client");
+console.error("Hiiii");
+console.warn("Warning");
+console.info("information");
+console.debug("debuging");
+document.write("bfghkjn");
+isworking=false;
+console.assert(isworking,"boolean");
+obj={
+    id:1,
+    name:"madhumitha",
+    Year:2024
+}
+console.log(obj);
+console.table(obj);
+//console.clear();
+a=10;//initialization
+
+//key variablename =value;
+//let var const
+
+var a=10450000.70;
+let b="anu";
+const c=30;
+var f='a';
+var a1=[10,20,30];
+document.write(a,b,c);
+
+document.write(`<br> a1[0]= ${a1[0]} a1[1]=${a1[1]} a1[2]=${a1[2]}`);
+
+let obj1=
+[ 
+    {
+    firstName:"ram",
+    secondName:"kumar",
+    age:25
+},
+{
+    firstName:"madhu",
+    secondName:"b",
+    age:25
+},
+{
+    firstName:"gayu",
+    secondName:"b",
+    age:25
+    
+}
+]
+
+console.log(obj1);
+//document.write("<br>age=",obj1.age,"<br>first name=",obj1.firstName,"<br>Secondname=",obj1.secondName);
+//templateliteral:  `${}` :ECMA :6
+//document.write(`<br> ${obj1.age} years age <br> first name= ${obj1.firstName}<br> Secondname ${obj1.secondName}`);
+let k=50;
+document.write(`<br> k=${k}`);
+document.write(`<br>   age ${obj1[1].age} <br> firstname${obj1[1].firstName}`);
